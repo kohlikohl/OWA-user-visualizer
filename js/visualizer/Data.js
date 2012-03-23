de7486f9056bf89aaf6935adf82abbd79a5f3418
Matthias Knoll <matthias@bravenewtalent.com>
2012-03-23 13:51:23 +0000

    updata dataset
    Signed-off-by: Matthias Knoll <matthias@bravenewtalent.com>

--- js/visualizer/Data.js
+++ js/visualizer/Data.js
@@ -4,16 +4,19 @@ function Data () {
 
 Data.prototype.loadData = function() {
     return {
+        current_timestamp: 1332508540,
         data: [
             {
                 sessionId: 100,
                 loggedIn: false,
                 pageViews: [
                     {
+                        id: 1,
                         target: 'homepage',
                         timestamp: 1332508172
                     },
                     {
+                        id: 2,
                         target: 'skills',
                         timestamp: 1332508440
                     }
