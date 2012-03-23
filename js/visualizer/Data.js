cec15e1cadd3b67d04f2629487da0ecff65c0407
Matthias Knoll <matthias@bravenewtalent.com>
2012-03-23 13:21:01 +0000

    added mock data
    Signed-off-by: Matthias Knoll <matthias@bravenewtalent.com>

--- js/visualizer/Data.js
+++ js/visualizer/Data.js
@@ -1,3 +1,24 @@
 function Data () {
-    
+    this.loadData();
+}
+
+Data.prototype.loadData = function() {
+    return {
+        data: [
+            {
+                sessionId: 100,
+                loggedIn: false,
+                pageViews: [
+                    {
+                        target: 'homepage',
+                        timestamp: 1332508172
+                    },
+                    {
+                        target: 'skills',
+                        timestamp: 1332508440
+                    }
+                ]
+            }
+        ]
+    };
 }
---
+++ js/visualizer/User.js
@@ -0,0 +1,5 @@
+function User (id, timestamp, target) {
+    this.id = id;
+    this.timestamp = timestamp;
+    this.target = target;
+}
